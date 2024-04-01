478ec04e3ef4426163bc63f9ace12e67e039741f
IshIsmael <150946594+IshIsmael@users.noreply.github.com>
2024-04-01 15:35:25 +0100

    js files for map, file upload preview and style files

---
+++ public/javascripts/maps.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var mymap = L.map('mapid').setView([53.3814, 1.4884], 13);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap contributors</a>',
+        maxZoom: 18,
+    }).addTo(mymap);
+    
+    var marker;
+
+    mymap.on('click', function(e) {
+        if (marker) mymap.removeLayer(marker);
+        marker = L.marker([e.latlng.lat, e.latlng.lng]).addTo(mymap);
+        document.getElementById('latitude').value = e.latlng.lat;
+        document.getElementById('longitude').value = e.latlng.lng;
+    });
+});
