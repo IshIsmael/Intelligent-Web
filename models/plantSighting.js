1145af9f66583a9549610e11ecdd5a2cfdf11a09
IshIsmael <150946594+IshIsmael@users.noreply.github.com>
2024-04-01 15:09:24 +0100

    Updated model schema

--- models/plantSighting.js
+++ models/plantSighting.js
@@ -1,46 +1,41 @@
-let mongoose = require('mongoose');
-let Schema = mongoose.Schema;
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
-// Schema for Comments Model
-const CommentSchema = new Schema({
-    message: { type: String, required: true, max: 100},
-    date: { type: Date, default: Date.now },
-    userNickname: { type: String, required: true}
+const plantSightingSchema = new Schema({
+    userNickname: { type: String, required: true },
+    dateSeen: { type: Date, required: true },
+    identification: {
+        commonName: { type: String },
+        scientificName: { type: String },
+        descriptions: { type: String, required: true },
+        dbPediaUri: { type: String },
+        photo: { type: String },
+        confirmation: { type: String, enum: ['Verified', 'Pending Confirmation'] },   
+    },
+    location: {
+        type: { type: String, enum: ['Point'], required: true },
+        coordinates: { type: [Number], required: true }
+    },
+    plantCharacteristics: {
+        plantHeight: { type: Number, required: true },
+        plantSpread: { type: Number, required: true },
+        hasFlowers: { type: Boolean, required: true },
+        hasFruitsorSeeds: { type: Boolean, required: true },
+        hasLeaves: { type: Boolean, required: true },
+        sunExposure: { type: String, enum: ['Full Sun', 'Partial Sun', 'Full Shade'], required: true },
+    },
+    comments: [
+        {
+            message: { type: String, required: true },
+            date: { type: Date, default: Date.now},
+            userNickname: { type: String, required: true }
+        }
+    ]
 });
 
-// Schema for PlantSighting Model
-const PlantSightingSchema = new Schema({
-    photo: { type: String, required: false},
-    identification:{
-        plantName: { type: String, required: false}, 
-        idConfirmation: { type: String, enum: ['Confirmed', 'Not Confirmed'], required: true}, 
-        dbPediaUrl: { type: String, required: false}
-    },
-    // GeoJSON for location
-    location:{
-        type: { type: String, default: 'Point'}, 
-        coordinates: { type: [Number], required: true} 
-    },
-    dateSeen: { type: Date, default: Date.now }, 
-    description: { type: String, required: false, max: 1000}, 
-    plantCharacteristics:{
-        height: { type: Number, required: true},
-        spread: { type: Number, required: true}, 
-        hasFlowers: { type: Boolean, required: true },
-        hasLeaves: { type: Boolean, required: true }, 
-        hasFruitsOrSeeds: { type: Boolean, required: true },
-        sunExposure: { type: String, enum: ['full sun', 'partial shade', 'full shade'], required: true },
-        flowerColor: { type: String, required: false }
-    },
-    userNickname: { type: String, required: true}, 
-    //CommentSchema nested
-    comments: [CommentSchema]
-}, {timestamps: true});
+// toObject() method is called when the document is converted into an object
+plantSightingSchema.set('toObject', { virtuals: true });
 
-// toObject transform
-PlantSightingSchema.set('toObject', { getters: true, virtuals: true });
+plantSightingSchema.index({ location: '2dsphere' });
 
-// for location based queries
-PlantSightingSchema.index({ location: '2dsphere' });
-
-module.exports = mongoose.model('PlantSighting', PlantSightingSchema);
+module.exports = mongoose.model('PlantSighting', plantSightingSchema);
