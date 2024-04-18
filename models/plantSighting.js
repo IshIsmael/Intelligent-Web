16ab68e22db12fe010d787077fe51c50d8a953e8
Luca <oppes.luca1@gmail.com>
2024-04-18 04:00:15 +0100

    Did some basic work on the front end

--- models/plantSighting.js
+++ models/plantSighting.js
@@ -2,36 +2,40 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const plantSightingSchema = new Schema({
-    userNickname: { type: String, required: true },
-    dateSeen: { type: Date, required: true },
-    identification: {
-        commonName: { type: String },
-        scientificName: { type: String },
-        description: { type: String, required: true },
-        dbPediaUri: { type: String },
-        photo: { type: String },
-        confirmation: { type: String, enum: ['Verified', 'Pending Confirmation'] },   
+  userNickname: { type: String, required: true },
+  dateSeen: { type: Date, required: true },
+  identification: {
+    commonName: { type: String },
+    scientificName: { type: String },
+    description: { type: String, required: true },
+    dbPediaUri: { type: String },
+    photo: { type: String },
+    confirmation: { type: String, enum: ['Verified', 'Pending Confirmation'] },
+  },
+  location: {
+    type: { type: String, enum: ['Point'], required: true },
+    coordinates: { type: [Number], required: true },
+  },
+  plantCharacteristics: {
+    plantHeight: { type: Number, required: true },
+    plantSpread: { type: Number, required: true },
+    plantLength: { type: Number, required: true },
+    hasFlowers: { type: Boolean, required: true },
+    hasFruitsOrSeeds: { type: Boolean, required: true },
+    hasLeaves: { type: Boolean, required: true },
+    sunExposure: {
+      type: String,
+      enum: ['full sun', 'partial shade', 'full shade'],
+      required: true,
     },
-    location: {
-        type: { type: String, enum: ['Point'], required: true },
-        coordinates: { type: [Number], required: true }
+  },
+  comments: [
+    {
+      message: { type: String, required: true },
+      date: { type: Date, default: Date.now },
+      userNickname: { type: String, required: true },
     },
-    plantCharacteristics: {
-        plantHeight: { type: Number, required: true },
-        plantSpread: { type: Number, required: true },
-        plantLength: { type: Number, required: true },
-        hasFlowers: { type: Boolean, required: true },
-        hasFruitsOrSeeds: { type: Boolean, required: true },
-        hasLeaves: { type: Boolean, required: true },
-        sunExposure: { type: String, enum: ['full sun', 'partial shade', 'full shade'], required: true },
-    },
-    comments: [
-        {
-            message: { type: String, required: true },
-            date: { type: Date, default: Date.now},
-            userNickname: { type: String, required: true }
-        }
-    ]
+  ],
 });
 
 // toObject() method is called when the document is converted into an object
--- routes/index.js
+++ routes/index.js
@@ -4,37 +4,51 @@ var multer = require('multer');
 const plantSightingController = require('../controllers/plantSighting');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Plant App' });
 });
+
 /* GET view plants page. */
-router.get('/view-plants', function(req, res, next) {
+router.get('/view-plants', function (req, res, next) {
   res.render('view-plants', { title: 'View Plants' });
 });
+
 /* GET create plant sighting page. */
-router.get('/create', function(req, res, next) {
+router.get('/create', function (req, res, next) {
   res.render('create', { title: 'Add a Plant' });
 });
+
 /* GET view sole plant info and chat page. */
-router.get('/plant-info', function(req, res, next) {
+router.get('/plant-info', function (req, res, next) {
   res.render('plant-info', { title: 'Plant Information' });
 });
+
 // Multer
 // Set up multer for file uploads
 const storage = multer.diskStorage({
-  destination: function(req, file, cb) {
+  destination: function (req, file, cb) {
     cb(null, 'public/images/uploads');
   },
-  filename: function(req, file, cb) {
+  filename: function (req, file, cb) {
     // Unique name for file
-    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
     // This is the file name that will be stored
-    cb(null, file.fieldname + '-' + uniqueSuffix + require('path').extname(file.originalname));
-  }
+    cb(
+      null,
+      file.fieldname +
+        '-' +
+        uniqueSuffix +
+        require('path').extname(file.originalname)
+    );
+  },
 });
 const upload = multer({ storage: storage });
 
 // Post request to submit plant sighting
-router.post('/submit-plant-sighting', upload.single('image'), plantSightingController.createSighting);
+router.post(
+  '/submit-plant-sighting',
+  upload.single('image'),
+  plantSightingController.createSighting
+);
 
 module.exports = router;
