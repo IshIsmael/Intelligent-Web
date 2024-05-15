f65e78595d895a5d887e5006087ca1f1baa67878
Luca <oppes.luca1@gmail.com>
2024-05-15 04:13:36 +0100

    network first service worker and cleaned up code for syncing offline created events

--- public/javascripts/create.js
+++ public/javascripts/create.js
@@ -1,137 +1,8 @@
+// Opening IndexedDB
 const sightingIndexedDB = window.indexedDB.open('sightings');
-sightingIndexedDB.addEventListener('upgradeneeded', handleUpgradeOne);
 sightingIndexedDB.addEventListener('success', handleSuccessOne);
+sightingIndexedDB.addEventListener('upgradeneeded', handleUpgradeOne);
 sightingIndexedDB.addEventListener('error', handleErrorOne);
-
-const submitForm = function (e) {
-  e.preventDefault();
-  e.stopImmediatePropagation();
-
-  const formdata = new FormData(e.target);
-
-  var object = {};
-  formdata.forEach((value, key) => (object[key] = value));
-  // var json = JSON.stringify(object);
-
-  const db = sightingIndexedDB.result;
-  // let nickName = document.getElementById('userNickname').value;
-  // let commonName = document.getElementById('commonName').value;
-  // let scientificName = document.getElementById('scientificName').value;
-  // let plantDescription = document.getElementById('description').value;
-  // let plantLength = document.getElementById('plantLength').value;
-  // let plantHeight = document.getElementById('plantHeight').value;
-  // let dateFound = document.getElementById('dateSeen').value;
-  // let fruitOrSeeds = document.getElementById('hasFruitsOrSeeds').value;
-  // let hasFlowers = document.getElementById('hasFlowers').value;
-  // let hasLeaves = document.getElementById('hasLeaves').value;
-  // let dbPedia = document.getElementById('dbPediaUri').value;
-  // let plantColour = document.getElementById('flowerColor').value;
-  // let plantSpread = document.getElementById('plantSpread').value;
-  // let sunExposure = document.getElementById('sunExposure').value;
-
-  const action = db.transaction('sightings', 'readwrite');
-  const store = action.objectStore('sightings');
-  // let queryData = {
-  //   nickname: nickName,
-  //   commonname: commonName,
-  //   scientificname: scientificName,
-  //   description: plantDescription,
-  //   length: plantLength,
-  //   height: plantHeight,
-  //   datefound: dateFound,
-  //   hasflowers: hasFlowers,
-  //   fruitorseeds: fruitOrSeeds,
-  //   hasleaves: hasLeaves,
-  //   dbpedia: dbPedia,
-  //   colour: plantColour,
-  //   exposure: sunExposure,
-  //   spread: plantSpread,
-  // };
-  let query = store.add(object);
-  query.onsuccess = function (event) {
-    console.log('added');
-    query.onerror = function (event) {
-      console.log(event.target.errorCode); //Produces an error if one occurs
-    };
-  };
-};
-
-function handleSuccessOne(ev) {
-  console.log('Opened..'); // USED FOR TESTING
-}
-
-function handleUpgradeOne(ev) {
-  const db = ev.target.result;
-  let store = db.createObjectStore('sightings', {
-    keyPath: 'id',
-    autoIncrement: true,
-  }); //Creates the object key
-  console.log('Upgraded object store...'); // USED FOR TESTING
-}
-
-function handleErrorOne(ev) {
-  console.error(`Database Error:`);
-}
-
-function nowOnline() {
-  console.log('window is online');
-
-  const db = sightingIndexedDB.result;
-  const objectStore = db
-    .transaction('sightings', 'readwrite')
-    .objectStore('sightings')
-    .getAll();
-
-  console.log(objectStore);
-
-  // objectStore.openCursor().onsuccess = async event => {
-  //   const cursor = event.target.result;
-  //   if (cursor) {
-  //     console.log(cursor);
-  //     let newSighting = {
-  //       dateSeen: new Date(cursor.value.datefound),
-  //       userNickname: cursor.value.nickname,
-  //       location: {
-  //         type: 'Point',
-  //         coordinates: [parseFloat('0.0'), parseFloat('0.0')],
-  //       },
-  //       identification: {
-  //         commonName: cursor.value.commonName,
-  //         scientificName: cursor.value.scientificName,
-  //         description: cursor.value.description,
-  //         dbPediaUri: cursor.value.dbpedia,
-  //         confirmation: 'Pending Confirmation',
-  //       },
-  //       plantCharacteristics: {
-  //         hasFlowers: cursor.value.hasflowers,
-  //         hsaLeaves: cursor.value.hasleaves,
-  //         hasFruitsOrSeeds: cursor.value.hasFruitsOrSeeds,
-  //         sunExposure: 'full sun',
-  //         flowerColor: 'kjn',
-  //         plantLength: 1,
-  //         plantHeight: 1,
-  //         plantSpread: 1,
-  //       },
-  //     };
-
-  //     console.log(newSighting);
-
-  objectStore.onsuccess = () => {
-    addToDb(objectStore.result[0]);
-    // console.log(objectStore.result[0]);
-  };
-
-  // addToDb(objectStore.result[1]);
-
-  //     // objectStore.delete(cursor.key);
-  //     cursor.continue();
-  //   } else {
-  //     console.log('no more sightings');
-  //   }
-  // };
-}
-
-window.addEventListener('online', nowOnline);
 
 async function addToDb(obj) {
   try {
@@ -148,4 +19,62 @@ async function addToDb(obj) {
   } catch (err) {
     console.log(err);
   }
+}
+
+const submitForm = function (e) {
+  e.preventDefault();
+  e.stopImmediatePropagation();
+
+  const newSighting = new FormData(e.target);
+  const sightingObj = {};
+  newSighting.forEach((value, key) => (sightingObj[key] = value));
+
+  if (navigator.onLine) {
+    addToDb(sightingObj);
+    window.location.href = '/forum';
+  } else {
+    const db = sightingIndexedDB.result;
+
+    const action = db.transaction('sightings', 'readwrite');
+    const store = action.objectStore('sightings');
+
+    let query = store.add(sightingObj);
+    query.onsuccess = function (event) {
+      console.log('added');
+      query.onerror = function (event) {
+        console.log(event.target.errorCode); //Produces an error if one occurs
+      };
+    };
+  }
+};
+
+window.addEventListener('online', syncIndexToMongo);
+
+function syncIndexToMongo() {
+  const db = sightingIndexedDB.result;
+  const objectStore = db
+    .transaction('sightings', 'readwrite')
+    .objectStore('sightings')
+    .getAll();
+
+  objectStore.onsuccess = () => {
+    addToDb(objectStore.result[0]);
+  };
+}
+
+function handleSuccessOne(ev) {
+  console.log('Opened..'); // USED FOR TESTING
+}
+
+function handleUpgradeOne(ev) {
+  const db = ev.target.result;
+  let store = db.createObjectStore('sightings', {
+    keyPath: 'id',
+    autoIncrement: true,
+  }); //Creates the object key
+  console.log('Upgraded object store...'); // USED FOR TESTING
+}
+
+function handleErrorOne(ev) {
+  console.error(`Database Error:`);
 }
--- public/sw.js
+++ public/sw.js
@@ -1,96 +1,30 @@
-// console.log('Service Worker Called...');
-
-// self.addEventListener('install', event => {
-//   console.log('[Service Worker] : Installing');
-//   event.waitUntil(
-//     caches.open('core').then(cache => {
-//       try {
-//         cache.addAll([
-//           '/',
-//           '../create',
-//           '../offline_plant_info',
-//           '/javascripts/index.js',
-//           '/javascripts/map.js',
-//           '/javascripts/script.js',
-//           '/javascripts/nickname.js',
-//           '/javascripts/forum.js',
-//           '/javascripts/plantDownloader.js',
-//           '/javascripts/offlinePlantInfo.js',
-//           '/stylesheets/footer.css',
-//           '/stylesheets/form.css',
-//           '/stylesheets/header.css',
-//           '/stylesheets/style.css',
-//           '/stylesheets/nickname.css',
-//         ]);
-//         console.log('[Service Worker] : Installed');
-//       } catch {
-//         console.log('Error Service Worker Install');
-//       }
-//     })
-//   );
-// });
-
-// // Fetch event to fetch from cache first
-// self.addEventListener('fetch', event => {
-//   console.log(event);
-//   event.respondWith(
-//     (async () => {
-//       const cache = await caches.open('core');
-//       const cachedResponse = await cache.match(event.request);
-//       if (cachedResponse) {
-//         console.log('Service Worker: Fetching from Cache: ', event.request.url);
-//         return cachedResponse;
-//       } else {
-//         console.log('Service Worker: Fetching from URL: ', event.request.url);
-//         return fetch(event.request);
-//       }
-//     })()
-//   );
-// });
-
 const addResourcesToCache = async resources => {
   const cache = await caches.open('v1');
   await cache.addAll(resources);
+};
+
+const putInCache = async (request, response) => {
+  if (request.method === 'POST' || request.method === 'PUT') return;
+
+  const cache = await caches.open('v1');
+  await cache.put(request, response);
+};
+
+const networkFirst = async request => {
+  try {
+    const responseFromNetwork = await fetch(request);
+    putInCache(request, responseFromNetwork.clone());
+    return responseFromNetwork;
+  } catch (err) {
+    const responseFromCache = await caches.match(request);
+    return responseFromCache;
+  }
 };
 
 self.addEventListener('install', event => {
   event.waitUntil(addResourcesToCache(['/']));
 });
 
-const putInCache = async (request, response) => {
-  const cache = await caches.open('v1');
-  await cache.put(request, response);
-};
-
-const cacheFirst = async request => {
-  const responseFromCache = await caches.match(request);
-  if (responseFromCache) {
-    return responseFromCache;
-  }
-  const responseFromNetwork = await fetch(request);
-  putInCache(request, responseFromNetwork.clone());
-  return responseFromNetwork;
-};
-
 self.addEventListener('fetch', event => {
-  event.respondWith(cacheFirst(event.request));
+  event.respondWith(networkFirst(event.request));
 });
-
-// self.addEventListener('fetch', event => {
-//   console.log(event.request.url);
-
-//   event.respondWith(
-//     (async () => {
-//       if (cachedResponse) {
-//         console.log('Service Worker: Fetching from Cache: ', event.request.url);
-//         return cachedResponse;
-//       } else {
-//         console.log('Service Worker: Fetching from URL: ', event.request.url);
-//         if (!event.request.url.includes('newMessage')) {
-//           addResourcesToCache([event.request.url]);
-//         }
-//         return fetch(event.request);
-//       }
-//     })()
-//   );
-// });
